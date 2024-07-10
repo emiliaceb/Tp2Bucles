7f7ebc633d66d5797cfad86442015e18ed2c2081
Emiliaceb <emiceballoss24@gmail.com>
2024-07-09 23:15:57 -0300

    ejercicio6 terminado

---
+++ js/06-ejercicio.js
@@ -0,0 +1,17 @@
+/*6- Realiza un script que escriba una pirámide del 1 al 30 de la siguiente forma :
+1
+22
+333
+4444
+55555
+666666
+…….*/
+let fila = '';
+
+for(i=1;i<31;i++){
+    for(b=0;b<i;b++){
+        document.write(i)
+       
+    }
+    document.write(`<br>`)
+}
